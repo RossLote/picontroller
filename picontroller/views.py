482fb0a15d3a33c50588882313622d72fc73c148
Ross Lote <rosslote@gmail.com>
2016-03-13 20:17:18 +0000

    update

--- picontroller/views.py
+++ picontroller/views.py
@@ -1,25 +1,25 @@
-from RPi import GPIO as pi
+# from RPi import GPIO as pi
 from django.views.generic import FormView
 from .forms import WheelSpeedForm
 from django.http import JsonResponse
-
-ALIVE = False
-
-PINS = [4, 17, 27, 22]
-
-pi.setmode(pi.BCM)
-for pin in PINS:
-    pi.setup(pin, pi.OUT)
-
-FORWARD_RIGHT_PIN = pi.PWM(PINS[0], 100)
-FORWARD_LEFT_PIN = pi.PWM(PINS[1], 100)
-BACKWARD_RIGHT_PIN = pi.PWM(PINS[2], 100)
-BACKWARD_LEFT_PIN = pi.PWM(PINS[3], 100)
-
-FORWARD_RIGHT_PIN.start(0)
-FORWARD_LEFT_PIN.start(0)
-BACKWARD_RIGHT_PIN.start(0)
-BACKWARD_LEFT_PIN.start(0)
+#
+# ALIVE = False
+#
+# PINS = [4, 17, 27, 22]
+#
+# pi.setmode(pi.BCM)
+# for pin in PINS:
+#     pi.setup(pin, pi.OUT)
+#
+# FORWARD_RIGHT_PIN = pi.PWM(PINS[0], 100)
+# FORWARD_LEFT_PIN = pi.PWM(PINS[1], 100)
+# BACKWARD_RIGHT_PIN = pi.PWM(PINS[2], 100)
+# BACKWARD_LEFT_PIN = pi.PWM(PINS[3], 100)
+#
+# FORWARD_RIGHT_PIN.start(0)
+# FORWARD_LEFT_PIN.start(0)
+# BACKWARD_RIGHT_PIN.start(0)
+# BACKWARD_LEFT_PIN.start(0)
 
 
 def turn_off(request):

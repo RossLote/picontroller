93ac9d7b70d0fd58fc11e6b729d0c7cecf22932d
Ross Lote <rosslote@gmail.com>
2016-03-13 18:14:14 +0000

    update

--- picontroller/urls.py
+++ picontroller/urls.py
@@ -20,7 +20,7 @@ from . import views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^$', TemplateView.as_view(template_name="index.html")),
+    url(r'^$', TemplateView.as_view(template_name="picontroller/index.html")),
     url(r'^start/$', views.turn_on),
     url(r'^stop/$', views.turn_off),
     url(r'^left/$', views.LeftWheelSpeed.as_view()),
